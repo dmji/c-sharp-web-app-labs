28eb0bd49ed3194ed7589e5568dfff94bf209778
daimonji <leyshkinanton@gmail.com>
2020-03-13 23:05:28 +0300

    chat-ready

--- tcp-client/tcp-client/Program.cs
+++ tcp-client/tcp-client/Program.cs
@@ -12,50 +12,83 @@ namespace SocketTcpClient {
         static string address = "127.0.0.1";                                                                // адрес сервера
         //static string address = "192.0.120.9";                                                                // адрес сервера
 
-        static void Main(string[] args) {
-            char ENDCHAR = 'R';
-            
-            IPHostEntry host1 = Dns.GetHostEntry("127.0.0.1");
-                Console.WriteLine(host1.HostName);
-            foreach (IPAddress ip in host1.AddressList)
-                Console.WriteLine(ip.ToString());
-                
-            while (ENDCHAR == 'R' || ENDCHAR == 'r' || ENDCHAR == 'к' || ENDCHAR == 'К') {
-                try {
+        public class asSoc
+        {
+            Socket handler;
 
-                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
-                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    socket.Connect(ipPoint);                                                                        // подключаемся к удаленному хосту
-                    Console.Write("Connected to server: " + ipPoint.ToString());
-                    while (socket.Connected == true) {
-                        //if(socket.re)
-                        Console.Write("\nEnter message:");
-                        string message = Console.ReadLine();
-                        byte[] data = Encoding.Unicode.GetBytes(message);
-                        socket.Send(data);
+            public asSoc(IPEndPoint e)
+            {
+                handler = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                handler.Connect(e);
+            }
 
-                        data = new byte[256]; // буфер для ответа
-                        StringBuilder builder = new StringBuilder();
-                        int bytes = 0; // количество полученных байт
-                        do {
-                            bytes = socket.Receive(data, data.Length, 0);
-                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                        } while (socket.Available > 0);
-                        Console.WriteLine("Server feedback: " + builder.ToString());
-                        if (message == "-close") {
-                            // закрываем сокет
-                            socket.Shutdown(SocketShutdown.Both);
-                            socket.Close();
-                            Console.WriteLine("Socet closed. Press 'R' to reconnect or any key to exit.\n");
-                            ENDCHAR = Console.ReadKey().KeyChar;
-                            Console.WriteLine("\n");
-                        }
+            public void Sender()
+            {
+                while (handler.Connected)
+                {
+                    //Console.Write("\n>> ");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    if (message == "-close")
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
                     }
-                }
-                catch (Exception ex) {
-                    Console.WriteLine(ex.Message);
+                    handler.Send(data);
                 }
             }
+            public void Listener()
+            {
+                while (handler.Connected)
+                {
+                    byte[] data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+                    do
+                    {
+                        bytes = handler.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        
+                    } while (handler.Available > 0);
+                    if(builder.ToString().Length>0)
+                        Console.WriteLine("<< " + builder.ToString());
+                    Thread.Sleep(100);
+
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            char ENDCHAR = 'R';
+
+            IPHostEntry host1 = Dns.GetHostEntry("127.0.0.1");
+            Console.WriteLine(host1.HostName);
+            foreach (IPAddress ip in host1.AddressList)
+                Console.WriteLine(ip.ToString());
+
+            asSoc socket = null;
+            //while (ENDCHAR == 'R' || ENDCHAR == 'r' || ENDCHAR == 'к' || ENDCHAR == 'К')            {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                socket = new asSoc(ipPoint);
+                Console.Write("Connected to server: " + ipPoint.ToString() + "\n");
+                Thread t1 = new Thread(socket.Sender);
+                t1.Start();
+                //listener
+                Thread t2 = new Thread(socket.Listener);
+                t2.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            /*
+                Console.WriteLine("\nSocet closed. Press 'R' to reconnect or any key to exit.\n");
+                ENDCHAR = Console.ReadKey().KeyChar;
+                Console.WriteLine("\n");
+            */
         }
     }
 }
--- tcp-server/tcp-server/Program.cs
+++ tcp-server/tcp-server/Program.cs
@@ -8,47 +8,80 @@ namespace SocketTcpServer {
     class Program {
         static int port = 8805; // порт для приема входящих запросов
 
-        public class asSoc {
+        public class asSoc
+        {
+            static System.Collections.Generic.List<string> msg = new System.Collections.Generic.List<string>();
             Socket handler;
             string name;
-            static int enter_count=1;
+            static int enter_count = 1;
             int id;
+            int msg_id;
 
             public asSoc(Socket e)
             {
+                msg_id = 0;
                 handler = e;
                 name = null;
                 id = enter_count;
                 enter_count++;
+                Console.WriteLine( "####" +handler.RemoteEndPoint.ToString() );
             }
 
-            public void AcceptCallback() {
+            public void Registration()
+            {
                 Console.WriteLine(">> Client #" + id + " connected first time");
+                byte[] data = new byte[256]; // буфер для получаемых данных
+                int bytes=0; // количество полученных байтов
+                StringBuilder builder = new StringBuilder();
+                data = Encoding.Unicode.GetBytes("Send name first.");
+                try
+                {
+                    handler.Send(data);
+                    do
+                    {
+                        bytes = handler.Receive(data);                                                                   // получаем сообщение
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (handler.Available > 0);
+                    name = builder.ToString();
+                    handler.Send(Encoding.Unicode.GetBytes("Name set as \'" + name + "\'.\n"));
+                    Console.WriteLine(">> Client #" + id + " registred as: " + name);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + "@Connection breaked.");
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    handler = null;
+                }
+                Thread t1 = new Thread(this.Listen);
+                t1.Start();
+                Thread t2 = new Thread(this.Sender);
+                t2.Start();
+            }
+
+            public void Listen()
+            {
+                StringBuilder builder = new StringBuilder();
                 byte[] data = new byte[256];
                 int bytes;
-                StringBuilder builder= new StringBuilder();
-                data = Encoding.Unicode.GetBytes("Send name first.");
-                handler.Send(data);
-                bytes = 0; // количество полученных байтов
-                data = new byte[256]; // буфер для получаемых данных
-                Console.WriteLine(">> Client #" + id + " registred as: " + name);
-                do
+                try                
                 {
-                    bytes = handler.Receive(data);                                                                   // получаем сообщение
-                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                } while (handler.Available > 0);
-                name = builder.ToString();
-                handler.Send(Encoding.Unicode.GetBytes("Name set as \'" + name + "\'.\n"));
-                try {
-                    while (handler!=null) {                        builder = new StringBuilder();
+                    while (handler != null)
+                    {
+                        builder = new StringBuilder();
                         bytes = 0; // количество полученных байтов
                         data = new byte[256]; // буфер для получаемых данных
-                        do {
+                        do
+                        {
                             bytes = handler.Receive(data);                                                                   // получаем сообщение
                             builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                            Thread.Sleep(100);
                         } while (handler.Available > 0);
-                        Console.WriteLine(DateTime.Now.ToShortTimeString() + " " + name + " : "  + builder.ToString());                    //вывод сообщения на сервере
-                        if (builder.ToString() == "-close") {
+                        string lmsg = DateTime.Now.ToShortTimeString() + " " + name + " : " + builder.ToString();
+                        Console.WriteLine(lmsg);                    //вывод сообщения на сервере
+                        msg.Add(lmsg);
+                        if (builder.ToString() == "-close")
+                        {
                             // закрываем сокет
                             string message = "@Connection closed.";
                             data = Encoding.Unicode.GetBytes(message);
@@ -57,14 +90,44 @@ namespace SocketTcpServer {
                             handler.Close();
                             handler = null;
                         }
-                        else {
-                            string message = "message back: # " + builder.ToString() + " #";
+                        else
+                        {
+                            string message = builder.ToString();
                             data = Encoding.Unicode.GetBytes(message);
                             handler.Send(data);
                         }
+
                     }
                 }
-                catch (Exception e) {
+                catch (Exception e)
+                {
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + "@Connection breaked.");
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    handler = null;
+                }
+               
+            }
+
+            public void Sender()
+            {
+                StringBuilder builder = new StringBuilder();
+                byte[] data = new byte[256];
+                int bytes;
+                try
+                {
+                    while (handler != null)
+                    {
+                        while (msg.Count > msg_id)
+                        {
+                            data = Encoding.Unicode.GetBytes(msg[msg_id++]);
+                            handler.SendTo(data, SocketFlags.None, handler.LocalEndPoint);
+                        }
+                        Thread.Sleep(100);
+                    }
+                }
+                catch (Exception e)
+                {
                     Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + "@Connection breaked.");
                     handler.Shutdown(SocketShutdown.Both);
                     handler.Close();
@@ -72,6 +135,7 @@ namespace SocketTcpServer {
                 }
             }
         }
+                
 
         static void Main(string[] args) {
             IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, port);                                                   // получаем адреса для запуска сокета
@@ -82,8 +146,9 @@ namespace SocketTcpServer {
 
             while (true) {
                 asSoc soc = new asSoc(listenSocket.Accept());
-                Thread t = new Thread(soc.AcceptCallback);
+                Thread t = new Thread(soc.Registration);
                 t.Start();
+               
             }
         }
     }
